2ffd83710f0aca991cd6700ed9b02a15e294e9a6
Tammi Nguyen <tnguyen7788>
2017-03-06 18:31:37 -0500

    Tammy's recent commit

--- weekend.js
+++ weekend.js
@@ -2,7 +2,23 @@ $(document).ready(function() {
    $('.inner-image').mouseenter(function() {
        $(this).fadeTo('fast',0.45)
      })
-     $('.inner-image').mouseout(function() {
+     $('.inner').mouseout(function() {
          $(this).fadeTo('fast',1)
        })
 });
+
+
+function myMove() {
+  var elem = document.getElementById("myAnimation");
+  var pos = 100;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 602) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
